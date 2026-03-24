1f938fa9c14b356cae2685c33db8822b56c45c0c
Vida96 <nikolavidovic47@gmail.com>
2026-03-24 16:31:50 +0100

    Fixed test. Added new lines to align with rules.

--- demo_project/calculator.py
+++ demo_project/calculator.py
@@ -2,13 +2,16 @@ def add(a, b):
     """Return the sum of a and b."""
     return a + b
 
+
 def subtract(a, b):
     """Return the difference between a and b."""
     return a - b
 
+
 def multiply(a, b):
     """Return the product of a and b."""
     return a * b
+
 
 def divide(a, b):
     """
